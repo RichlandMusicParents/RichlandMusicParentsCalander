9e54d1a108d00320608249fc4d7e0eace519ff6e
Mustafa <MustafaGino1@yahoo.com>
2023-03-17 15:13:08 -0500

    Removed unfinished features on userForm.

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -48,16 +48,16 @@ function UserForm() {
 
   }, []);
 
-  const eventHandleSubmit = () => {
-    dispatch({
-        type:`USER_ADD_EVENT`,
-        payload: {
-            eventOption,
-            date,
-            eventFor
+  // const eventHandleSubmit = () => {
+  //   dispatch({
+  //       type:`USER_ADD_EVENT`,
+  //       payload: {
+  //           eventOption,
+  //           date,
+  //           eventFor
 
-        },
-    });
+  //       },
+  //   });
 
   const handleSubmit = () => {
 
@@ -225,7 +225,7 @@ setTotal("")
             <Button onClick={handleSubmit}> Check Out </Button>
 
           
-            <Button onClick={eventHandleSubmit} > Add Event </Button>
+            <Button > Add Event </Button>
 
             <br />
             <br/>
@@ -250,6 +250,6 @@ setTotal("")
       </Grid>
     </div>
   );
-}}
+}
 
 export default UserForm;
