ff59379f52bb4b425b3e83da8d8428b65e64342e
Maya <DahirowSumaia@gmail.com>
2023-03-23 16:04:00 -0500

    made changes to the events router and event form to get orderid

--- server/routes/orderitems.router.js
+++ server/routes/orderitems.router.js
@@ -7,19 +7,21 @@ const {
 
 
 router.get('/', (req, res) => {
-    const queryText = 'SELECT * FROM "order_items"';
+
+    const queryText = `SELECT * FROM "order_items"; `
     pool.query(queryText)
       .then((result) => {
         res.send(result.rows);
       })
       .catch((error) => {
-        console.log('Error getting order', error);
+        console.log('Error getting order_items', error);
         res.sendStatus(500);
       });
   });
 
 
   router.post("/", (req, res) => {
+   
     const queryText = `
     INSERT INTO order_items (quantity, price, product_id, order_id, user_id)
     VALUES ($1, $2, $3, $4, $5);`;
