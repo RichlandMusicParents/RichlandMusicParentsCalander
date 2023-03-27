41963c47f9a51050b9804908fd3e433eec90c157
Maya <DahirowSumaia@gmail.com>
2023-03-27 10:52:46 -0500

    added a thank you message to the dialog alert

--- src/components/Pages/ConfirmDialog/ConfirmDialog.jsx
+++ src/components/Pages/ConfirmDialog/ConfirmDialog.jsx
@@ -6,57 +6,70 @@ import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 import Button from "@mui/material/Button";
 import { useHistory } from "react-router-dom";
+import { textAlign } from "@mui/system";
 
 function ConfirmDialog() {
-    const history = useHistory();
-    const [open, setOpen] = useState(false);
-    const [showConfirmation, setShowConfirmation] = useState(true);
-    const [showThankYou, setShowThankYou] = useState(false);
+  const history = useHistory();
+  const [open, setOpen] = useState(false);
+  const [showConfirmation, setShowConfirmation] = useState(true);
+  const [showThankYou, setShowThankYou] = useState(false);
 
-const handleClickOpen = () => {
-    setOpen(true)
-};
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-const handleClose = () => {
-setOpen(false)
-};
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-const handleConfirm = () => {
+  const handleConfirm = () => {
     setShowConfirmation(false);
+    setShowThankYou(true);
     setTimeout(() => {
-        history.push("/Complete")
+      history.push("/Complete");
     }, 5000);
+  };
 
-};
-
-    return (
-        <>
-    <Button onClick={handleClickOpen}> Complete Order</Button>
-        <Dialog
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="alert-title"
-            aria-descrivedby="alert-description"
-        >
-            {showConfirmation && ( 
-                <>
-     <DialogTitle id="alert-title"> {"Confirm Order?"}</DialogTitle>
-     <DialogContent id="alert-description"> 
-     Are you sure you want to complete the order? 
-     </DialogContent>
-     <DialogActions>
-        <Button onClick={handleClose}>No</Button>
-        <Button onClick={handleConfirm} autofocus>Yes</Button>
-
-     </DialogActions>
-            </>
-            )}
-            <DialogContent>
-                
+  return (
+    <>
+      <Button onClick={handleClickOpen}> Complete Order</Button>
+      <Dialog
+      sx={{textAlign:"center"}}
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-title"
+        aria-descrivedby="alert-description">
+        {showConfirmation && (
+          <>
+            <DialogTitle id="alert-title"> {"Confirm Order?"}</DialogTitle>
+            <DialogContent id="alert-description">
+              Are you sure you want to complete the order?
             </DialogContent>
-        </Dialog>
-        </>
-    );
-};
+            <DialogActions>
+              <Button onClick={handleClose}>No</Button>
+              <Button onClick={handleConfirm} autofocus>
+                Yes
+              </Button>
+            </DialogActions>
+          </>
+        )}
+
+        {showThankYou && (
+          <DialogContent>
+             <DialogTitle style={{ color: "green"}} id="alert-title"  
+             alignItems="center"
+             justifyContent="center"
+             > {"Thank for your order!"}</DialogTitle>
+            <DialogContentText   
+            justifyContent="center"
+            alignItems="center" >
+            Please wait while we are processing your order!
+            </DialogContentText>
+          </DialogContent>
+        )}
+      </Dialog>
+    </>
+  );
+}
 
 export default ConfirmDialog;
