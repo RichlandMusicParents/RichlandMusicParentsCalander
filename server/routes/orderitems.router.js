940836ff7e8c310b1791da008ab3cddceebede14
Mustafa <MustafaGino1@yahoo.com>
2023-03-28 16:28:38 -0500

    get,put,delete route and saga set up for extra events/calendars. user is able to add,delete and update extra events and calendars to their liking.

--- server/routes/orderitems.router.js
+++ server/routes/orderitems.router.js
@@ -5,19 +5,27 @@ const {
   rejectUnauthenticated,
 } = require("../modules/authentication-middleware");
 
-router.get("/", (req, res) => {
-  const queryText = `SELECT * FROM "order_items"; `;
+
+router.get("/:id", rejectUnauthenticated, (req, res) => {
+  const text = `SELECT
+	*, 
+	(SELECT 
+	"product"."name" FROM "product" WHERE "product".id = "order_items"."product_id" )
+FROM
+	order_items
+WHERE
+	user_id = $1;
+`;
   pool
-    .query(queryText)
-    .then((result) => {
-      res.send(result.rows);
+    .query(text, [req.params.id])
+    .then((results) => {
+      res.send(results.rows);
     })
-    .catch((error) => {
-      console.log("Error getting order_items", error);
+    .catch((err) => {
+      console.log("Error getting specific order items", err);
       res.sendStatus(500);
     });
 });
-
 router.post("/", (req, res) => {
   const queryText = `
     INSERT INTO order_items (quantity, price, product_id, order_id, user_id)
@@ -36,4 +44,44 @@ router.post("/", (req, res) => {
     });
 });
 
+
+//Delete order-item by it's id.
+router.delete("/delete-order-item/:id", rejectUnauthenticated, (req, res) => {
+  const text = `DELETE FROM "order_items" WHERE "id" = $1;`;
+
+  pool
+    .query(text, [req.params.id])
+    .then((result) => {
+      res.sendStatus(204);
+    })
+    .catch((err) => {
+      console.log("Error in DELETEing order item", err);
+      res.sendStatus(500);
+    });
+});
+
+//Edit order items by its id.
+router.put("/edit-order-items/:id", rejectUnauthenticated, (req, res) => {
+  const queryText = `UPDATE "order_items" SET "quantity" = $1, "price" = $2, "product_id" = $3, "order_id" = $4, "user_id" = $5
+	WHERE "id" = $6;`;
+
+  const { quantity, price, product_id, order_id, user_id } = req.body;
+
+  pool
+    .query(queryText, [
+      quantity,
+      price,
+      product_id,
+      order_id,
+      user_id,
+      req.params.id,
+    ])
+    .then((result) => {
+      res.send(result.rows[0]);
+    })
+    .catch((err) => {
+      console.error("Error in editing order items", err);
+      res.sendStatus(500);
+    });
+});
 module.exports = router;
--- src/redux/reducers/orderitems.reducer.js
+++ src/redux/reducers/orderitems.reducer.js
@@ -2,8 +2,6 @@ const orderItemsReducer = (state = [], action) => {
   switch (action.type) {
     case "SET_ORDER_ITEMS":
       return action.payload;
-    case "ADD_ORDER_ITEMS":
-      return [...state, action.payload];
     default:
       return state;
   }
