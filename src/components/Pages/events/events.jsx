9186e5bd4942855691f07ee9772f079ad8faae23
Brock Benson <brockjbenson@outlook.com>
2023-04-06 13:24:04 -0500

    events fixed

--- src/components/Pages/events/events.jsx
+++ src/components/Pages/events/events.jsx
@@ -29,9 +29,7 @@ import {
   Grid,
 } from "@mui/material";
 
-
 import "../Admin/AdminAddEvents.css";
-
 
 function Events() {
   const history = useHistory();
@@ -48,7 +46,6 @@ function Events() {
   const orders = useSelector((store) => store.order.newOrder);
   const orderItems = useSelector((store) => store.orderItemsReducer);
 
-
   //UseState for event form
   const [eventFor, setEventFor] = useState("");
   const [numCalendars, setNumCalendars] = useState(0);
@@ -58,11 +55,6 @@ function Events() {
   const [price, setPrice] = useState(0);
   const [eventOption, setEventOption] = useState("0");
   const [date, setDate] = useState("");
-  const [events, setEvents] = useState("");
-  const [numEvents, setNumEvents] = useState(0);
-  // const [quantity, setQuantity] = useState(0);
-  const [total, setTotal] = useState(0);
-  const [name, setName] = useState(null);
 
   // ORDER ITEMS
   // const [quantity, setQuantity] = useState(1);
@@ -136,8 +128,6 @@ function Events() {
     setEventEditId(false);
   }
 
-
-
   // TESTING TOTAL
   useEffect(() => {
     orders[0] !== undefined && orders[0].id;
@@ -197,303 +187,6 @@ function Events() {
     },
   });
 
-
-  return (
-    <div className="invoice-container">
-      <ThemeProvider theme={richlandTheme}>
-        <h1 className="form-title">Richland Music Parents</h1>
-        <Card
-          className="form-card"
-          sx={{
-            marginBottom: "50px",
-            borderRadius: "4px",
-          }}>
-          <Grid container spacing={3}>
-            <Grid item xs={12}>
-              <u>
-                <h1
-                  style={{
-                    fontSize: "40px",
-                    marginBottom: "1rem",
-                    textAlign: "center",
-                  }}>
-                  Event Form
-                </h1>
-              </u>
-              <h3 style={{ textAlign: "center" }}>2/3 Completed</h3>
-              <hr />
-              <br />
-              <br />
-            </Grid>
-            <Grid item xs={6}>
-              <CardContent>
-                <FormControl fullWidth sx={{ width: "100%" }}>
-                  <InputLabel htmlFor="selectCalendarId">
-                    {" "}
-                    Calendar Year{" "}
-                  </InputLabel>
-                  <Select
-                    fullWidth
-                    className="calendar-dropdown"
-                    name="selectedCaledarId"
-                    onChange={(event) =>
-                      setSelectedCalendarID(event.target.value)
-                    }
-                    id="calendar"
-                    value={selectCalendarId}>
-                    {calendars.map((calendar) => (
-                      <MenuItem value={calendar.id} key={calendar.id}>
-                        {calendar.calendar_name}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-              </CardContent>
-            </Grid>
-
-            <Grid item xs={6}>
-              <CardContent>
-                <TextField
-                  sx={{ width: "100%" }}
-                  fullWidth
-                  label="Event for"
-                  type="text"
-                  name="Event for"
-                  value={eventFor}
-                  required
-                  onChange={(event) => setEventFor(event.target.value)}
-                />
-              </CardContent>
-            </Grid>
-
-            <Grid item xs={6}>
-              <CardContent>
-                <TextField
-                  sx={{ width: "100%" }}
-                  type="date"
-                  value={date}
-                  onChange={(e) => setDate(e.target.value)}
-                />
-              </CardContent>
-            </Grid>
-
-            <Grid item xs={6}>
-              <CardContent>
-                <FormControl sx={{ width: "100%" }}>
-                  <InputLabel id="event-label"> Event Options</InputLabel>
-                  <Select
-                    sx={{ width: "100%" }}
-                    renderInput={(params) => (
-                      <TextField {...params} label="Event Type" />
-                    )}
-                    name="event_type"
-                    id="eType"
-                    value={eventOption}
-                    onChange={(e) => setEventOption(e.target.value)}>
-                    <MenuItem value="0">Select Event Type</MenuItem>
-                    <MenuItem value="birthday">Birthday</MenuItem>
-                    <MenuItem value="anniversary">Anniversary</MenuItem>
-                    <MenuItem value="memorial">Memorial</MenuItem>
-                  </Select>
-                </FormControl>
-                <Grid item xs={12} sm={6} md={2}>
-                  <Button
-                    sx={{
-                      display: "flex",
-                      justifyContent: "center",
-                      backgroundColor: "#4caf50",
-                      padding: "7px",
-                      color: "white",
-                      fontSize: "1.2rem",
-                      height: "100%",
-                      fontWeight: "600",
-                      boxShadow: "none",
-                      marginTop: "2rem",
-                      borderRadius: "50px",
-                      "&:hover": {
-                        backgroundColor: "#4caf50",
-                      },
-                    }}
-                    onClick={eventHandleSubmit}>
-                    {" "}
-                    Add Event{" "}
-                  </Button>
-                </Grid>
-              </CardContent>
-            </Grid>
-
-            <Grid container spacing={3}>
-              <Grid item xs={8}>
-                <UserCartComponent />
-              </Grid>
-              <Grid>
-                <article
-                  className="cart-icon-container"
-                  style={{
-                    padding: "1rem",
-                    borderRadius: "4px",
-
-                    marginLeft: "0px",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "flex-start",
-                  }}>
-                  {products.map((product) => (
-                    <>
-                      {!orderItems.some(
-                        (item) => item.product_id === product.id
-                      ) ? (
-                        <>
-                          <div
-                            key={product.id}
-                            className="product-item"
-                            style={{ marginBottom: "1rem" }}>
-                            <h3
-                              style={{
-                                fontWeight: "bold",
-                                fontSize: "1.2rem",
-                              }}>
-                              {product.name}: ${product.price}
-                            </h3>
-                            <GoPlus
-                              className="product-cart-icon cart-plus"
-                              onClick={() =>
-                                addItems(product.id, product.price)
-                              }
-                            />
-                          </div>
-                        </>
-                      ) : (
-                        <>
-                          <div
-                            key={product.id}
-                            className="product-cart-item "
-                            style={{ marginBottom: "1rem" }}>
-                            <h3
-                              style={{
-                                fontWeight: "bold",
-                                fontSize: "1.2rem",
-                              }}>
-                              {product.name}: ${product.price}
-                            </h3>
-                            <BsCartCheckFill className="product-cart-icon" />
-                          </div>
-                        </>
-                      )}
-                    </>
-                  ))}
-                </article>
-              </Grid>
-            </Grid>
-            <div
-              className="event-details"
-              style={{
-                width: "100%",
-                marginLeft: "1rem",
-                marginRight: "-1rem",
-                marginTop: "-500px",
-              }}>
-              <u>
-                <h1
-                  style={{
-                    fontSize: "40px",
-                    marginBottom: "1rem",
-                    textAlign: "center",
-                  }}>
-                  Event Details
-                </h1>
-              </u>
-              <Paper
-                elevation={12}
-                className="event-table"
-                style={{ padding: "1rem" }}>
-                <TableContainer component={Paper}>
-                  <Table>
-                    <TableHead>
-                      <TableRow>
-                        <TableCell
-                          style={{ fontWeight: "bold", fontSize: "1.2rem" }}>
-                          Date
-                        </TableCell>
-                        <TableCell
-                          style={{ fontWeight: "bold", fontSize: "1.2rem" }}>
-                          Event Type
-                        </TableCell>
-                        <TableCell
-                          style={{ fontWeight: "bold", fontSize: "1.2rem" }}>
-                          Name
-                        </TableCell>
-                        <TableCell
-                          style={{ fontWeight: "bold", fontSize: "1.2rem" }}>
-                          Delete
-                        </TableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {event.map((event) => (
-                        <TableRow key={event.id}>
-                          <TableCell style={{ fontSize: "1.2rem" }}>
-                            {formatDate(event.event_date)}
-                          </TableCell>
-                          <TableCell style={{ fontSize: "1.2rem" }}>
-                            {event.event_type}
-                          </TableCell>
-                          <TableCell style={{ fontSize: "1.2rem" }}>
-                            {event.event_name}
-                          </TableCell>
-                          <TableCell>
-                            <Button
-                              color="primary"
-                              variant="contained"
-                              sx={{
-                                backgroundColor: "#b80000",
-                                color: "white",
-                                fontSize: "1.2rem",
-                                fontWeight: "600",
-                                padding: "1rem 2rem",
-                                boxShadow: "none",
-                                marginTop: "2rem",
-                                borderRadius: "50px",
-                                "&:hover": {
-                                  backgroundColor: "#bf4040",
-                                },
-                              }}
-                              onClick={() => deleteUserEvent(event.id)}>
-                              Delete
-                            </Button>
-                          </TableCell>
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                </TableContainer>
-              </Paper>
-              <Button
-                color="primary"
-                variant="contained"
-                sx={{
-                  backgroundColor: "#4caf50",
-                  color: "white",
-                  height: "65px",
-                  fontSize: "1.2rem",
-                  marginLeft: "900px",
-                  fontWeight: "600",
-                  boxShadow: "none",
-                  marginTop: "2rem",
-                  marginBottom: "3rem",
-                  borderRadius: "50px",
-                  "&:hover": {
-                    backgroundColor: "#4caf50",
-                  },
-                }}
-                onClick={handleCheckout}>
-                {" "}
-                Check Out{" "}
-              </Button>
-            </div>
-          </Grid>
-        </Card>
-=======
   const [eventType, setEventType] = useState("0");
   const [eventDate, setEventDate] = useState("");
   const [eventName, setEventName] = useState("");
@@ -775,7 +468,6 @@ function Events() {
             </TableContainer>
           </Paper>
         </section>
-
       </ThemeProvider>
     </>
   );
